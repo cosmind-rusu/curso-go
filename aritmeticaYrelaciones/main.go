4b6ae8135f65ea1efd54d069082e385186f3bc45
cosmind-rusu <cosminrusu014@gmail.com>
2023-01-01 12:22:13 +0100

    ARITMETICA Y RELACIONALES

---
+++ aritmeticaYrelaciones/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// #################################### OPERADORES ARITMETICOS ####################################
+
+	// CREAMOS VARIABLES
+	var a, b int
+	// ENTRADA DE DATOS
+	fmt.Print("Ingrese el primer numero: ")
+	fmt.Scanln(&a)
+	fmt.Print("Ingrese el segundo numero: ")
+	fmt.Scanln(&b)
+
+	// PROCESOS
+	suma := a + b
+	resta := a - b
+	multiplicacion := a * b
+	division := a / b
+	resto := a % b
+
+	// SALIDA DE DATOS
+	fmt.Println("La suma es: ", suma)
+	fmt.Println("La resta es: ", resta)	
+	fmt.Println("La multiplicacion es: ", multiplicacion)
+	fmt.Println("La division es: ", division)
+	fmt.Println("El resto es: ", resto)
+
+// #################################### OPERADORES ARITMETICOS ####################################
+
+// #################################### OPERADORES RELACIONALES ####################################
+
+	// CREAMOS VARIABLES
+	var c, d int
+	// ENTRADA DE DATOS
+	fmt.Println("Ingresa el primer numero: ")
+	fmt.Scanln(&c)
+	fmt.Print("Ingrese el segundo numero: ")
+	fmt.Scanln(&d)
+
+	// PROCESOS
+	
+
+	// SALIDA DE DATOS
+
+}
