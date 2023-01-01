e4d112ab1eda9a7b31bfcdcecc903985c64fe0c5
cosmind-rusu <cosminrusu014@gmail.com>
2023-01-01 11:58:20 +0100

    Entrada de datos con scan

--- salidasPorPantalla/main.go
+++ salidasPorPantalla/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main () {
 	// ESTE PARAMETRO NO HACE SALTO DE LINEA
@@ -21,5 +23,28 @@ func main () {
 	%d para mostrar datos de tipo entero
 	%f paÍa mostrar datos de tipo flotante */
 
-	fmt.Printf("Nombre: %s Edad: %d \nPi: %f", nombre, edad, pi)
+	fmt.Printf("Nombre: %s Edad: %d \nPi: %f \n", nombre, edad, pi)
+
+	//ahora pasaremos datos por teclado
+
+	var (
+		name string
+		age int
+	)
+
+	// IMPRIMIMOS UN MENSAJE EN PANTALLA SIN SALTO DE LINEA
+	
+	// USAMOS ESTO PARA PODER RECOGER DATOS POR TECLADO
+	// CON LOS METODOS QUE HEMOS USAUDO AHORA DEBEREMOS DE INDICAR DE QUE TIPO ES EL DATO QUE VAMOS A RECOGER CON LOS PARAMETROS %s, %d, %f
+
+	fmt.Print("Ingrese su Nombre: ")
+	fmt.Scanln(&name)
+	/* fmt.Scanf("%s", name) */
+	fmt.Print("Ingrese su Edad: ")
+	fmt.Scanln(&age)
+	/* fmt.Scanf("%d", age) */
+									
+	// PERO TENEMOS OTRO METODO MAS SENCILLO DE HACERLO CON fmt.Scanln
+
+
 }
