49d9aa3abe487101ecfab2fd8671c7c05de94bb2
cosmind-rusu <cosminrusu014@gmail.com>
2023-01-06 15:36:48 +0100

    Asignacion e incremento

--- operadoresAsignacion/main.go
+++ operadoresAsignacion/main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
- fmt.Println("Hola")
+	fmt.Println("OPERADORES DE ASIGNACION")
+ 	a := 5
+	//a = a + 3 // Los operadores en asignacion simplifican las operaciones ANTES
+	a += 3 // Los operadores en asignacion simplifican las operaciones DESPUES
+	fmt.Println(a)
+	fmt.Println("OPERADORES DE ASIGNACION")
+
+	fmt.Println("\n")
+
+	fmt.Println("OPERADORES DE INCREMENTO")
+	//operadores de incremento
+	b := 1
+	b++
+	fmt.Println(b)
+	for b := 0; b < 4; b++ { // ++ es una instrucción.
+        fmt.Println(b)
+    }
+	fmt.Println("OPERADORES DE INCREMENTO")
 }
